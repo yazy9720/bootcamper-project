73efcfa39131656fb6790d80074d518029e93a89
Matthew Dowell <mbd06b@gmail.com>
2021-05-24 12:03:11 -0500

    loop and stream based mock DAOs

--- src/main/java/com/organization/mvcproject/service/GameServiceImpl.java
+++ src/main/java/com/organization/mvcproject/service/GameServiceImpl.java
@@ -1,84 +1,38 @@
 package com.organization.mvcproject.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.organization.mvcproject.dao.GameStreamBasedDAO;
 import com.organization.mvcproject.model.Game;
+
 
 @Service
 public class GameServiceImpl implements GameService {
 
-	private static Long gameId = new Long(0);
-	private static Long companyId = new Long(0);
-	private static List<Game> games = new ArrayList<Game>();
+	@Autowired
+	private GameStreamBasedDAO gameDAO; 
 
-	static {
-		games = populateGames();
-	}
-
-	private static List<Game> populateGames() {
-
-		Game game1 = new Game();
-		game1.setId(++gameId);
-		game1.setGenre("Sport");
-		game1.setName("Rocket League");
-
-		Game game2 = new Game();
-		game2.setId(++gameId);
-		game2.setGenre("Shooter");
-		game2.setName("Halo 3");
-
-		Game game3 = new Game();
-		game3.setId(++gameId);
-		game3.setGenre("MMORPG");
-		game3.setName("Runescape");
-
-		games.add(game1);
-		games.add(game2);
-		games.add(game3);
-
-		return games;
-	}
-
-	@Override
+	
 	public List<Game> retrieveAllGames() {
-		return games;
+		return gameDAO.findAllGames();
 	}
 
-	@Override
+	
 	public Game saveGame(Game game) {
-		game.setId(++gameId);
-		games.add(game);
-		return game;
-	}
-	
-
-	public static Long getGameId() {
-		return gameId;
-	}
-
-	public static void setGameId(Long gameId) {
-		GameServiceImpl.gameId = gameId;
-	}
-
-	public static Long getCompanyId() {
-		return companyId;
-	}
-
-	public static void setCompanyId(Long companyId) {
-		GameServiceImpl.companyId = companyId;
-	}
-
-	public static List<Game> getGames() {
-		return games;
-	}
-
-	public static void setGames(ArrayList<Game> games) {
-		GameServiceImpl.games = games;
+		return gameDAO.saveGame(game);
 	}
 	
 	
+	public Game findGameById(Long id) {
+		return gameDAO.findGameById(id);
+	}
 
+	
+	public boolean deleteGame(Long id) {
+		return gameDAO.deleteGame(id); 
+	}
 }
+	
